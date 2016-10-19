b105bb13a63507efe6473e4b109721444753eff4
Thomas Pulzer <t.pulzer@thesecretgamer.de>
2016-10-19 15:37:43 +0200

    Added fallback for including navigation through javascript. Added noscript warning

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 var content = document.querySelector('link[rel="import"]').import;
-var navigation = content.querySelector('#menubar');
-document.querySelector('#site-header').appendChild(navigation.cloneNode(true));
+if(content !== undefined) {
+    var navigation = content.querySelector('#menubar');
+    document.querySelector('#site-header').appendChild(navigation.cloneNode(true));
+} else {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "fragments/navbar.html", true);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            var allText = rawFile.responseText;
+            document.querySelector('#site-header').innerHTML = allText;
+        }
+    };
+    rawFile.send();
+}
 
 Array.from(document.getElementsByTagName("li")).forEach(function(item) {
     if(document.location.href.match(new RegExp(item.id,'i'))) {
